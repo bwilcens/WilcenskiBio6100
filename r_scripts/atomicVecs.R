186d960517c3174b851f29d85470aa85985b43c6
Bryan Wilcenski <bwilcens@uvm.edu>
2026-01-27 21:15:35 -0500

    Update atomicVecs.R

--- r_scripts/atomicVecs.R
+++ r_scripts/atomicVecs.R
@@ -15,7 +15,7 @@ plantHeight <- 4 # camel case
 plant.height <- 2 # this is NOT preferred 
 
 # reserved in R (F,T)
-. #reserved for temporary variable
+ #reserved for temporary variable
 
 # Four basic data types
 
@@ -30,7 +30,7 @@ z <- c(3.2,5,5,6)
 print(z)
 typeof(z) #identify the data type (float, double in this case)
 
-z <- c(c(3.2,3),(3,5))
+
 print(z)
 is.numeric(z) # will return F if not
 is.character(z)
@@ -125,3 +125,148 @@ x<-c(1,2)
 
 z + x
 
+####################################
+#atomic vecs part II
+
+z <- vector( mode ="numeric", length = 0)
+print(z)
+
+#dynamic creation (don't in R, do in python)
+z<- c(z,5)
+print(z)
+
+#predefined length
+
+z <- rep(0,100)
+print(z)
+length(z)
+
+## can create the empty list to speed up processing in for loops
+
+z<- rep(NA, 100)
+z
+
+typeof(z)
+
+z[1] <- "vermont"
+head(z)
+typeof(z)
+
+
+my_vector <- runif(100)
+my_names <- paste("species", seq(1:length(my_vector)),sep="")
+print(my_names)
+
+names(my_vector) <- my_names
+head(my_vector)
+str(my_vector)
+
+
+#using the rep function
+
+rep(0.5,6)
+rep(x=0.5, times =6) # should write out the arguments in functions I am using
+
+my_vec <- c(1,2,3)
+
+#repeat a vector
+rep(x=my_vec, times = 2, each =2)
+
+#sequencing vectors 
+seq(from = 2, to = 4)
+2:4
+
+x<- seq(from = 2, to = 4, length=7) #evenly space w a given start/end/length
+
+my_vec <- 1:length(x) # common in other languages, slow in R 
+my_vec
+
+#better in R to use: 
+seq_along(my_vec)
+
+# generate random vectors
+
+runif(5) #gives us 5 values from 0:1 
+#the params
+runif(n=3, min=100,max=101)
+
+set.seed(123) #takes any number 
+
+#gives you the same progression of random numbers
+runif(n=3, min=0,max=1)
+
+#normal distribution
+out<-rnorm(n=500, mean= 100, sd=30)
+hist(out)
+
+# random sampling 
+
+long_vec <- seq_len(10)
+long_vec
+
+#can set seed if you want as well 
+
+sample(x=long_vec, size = 10, replace=T)
+
+#weighted sampling from a vec
+weights <- c(rep(90,5), rep(10,5))
+weights
+
+sample(x=long_vec,replace=TRUE, prob=weights)
+
+
+#subsetting vectors
+z <- c(3.1, 9.2, 1.3, 0.4,7.5)
+print(z)
+
+z[-c(2,3)] #using vecs slice
+
+#using logicals 
+z[!z<3] # = z[z>3]
+
+#relational operators
+#<
+#>
+#<=
+#>=
+#    == (need to use two of them for 'equals')
+
+#logical operators
+# 1 == not 
+# & == and (vector)
+# | == or (vector)
+# xor == (x,y)     {exclusive or; will return true if a condition is only true for one of two items in vector}
+
+
+x<-1:5
+y<- c(1:3,7,7)
+
+x==2 #equals 
+x!=2 #not equal
+
+x== 1 & y ==7 #using and: never happens in defined sequences ^^^
+
+x == 1 | y == 7 # using or
+
+x==3 & y==3 # using or
+
+xor(x==3,y==5) # exclusive or (xor)
+
+
+# missing values
+
+set.seed(90)
+z<- runif(10)
+z
+z<0.5
+z[z<0.5]
+z[which(z<0.5)]
+
+zd <- c(z,NA, NA)
+zd
+
+zd[zd<0.5]
+
+#dropping NAs with which to index
+
+zd[which(zd<0.5)]
