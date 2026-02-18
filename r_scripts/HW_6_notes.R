f9d802d56f9358f4ffe4f51bc99b2b38e19c64a5
Bryan Wilcenski <bwilcens@uvm.edu>
2026-02-18 14:53:00 -0500

    homework 06

---
+++ r_scripts/HW_6_notes.R
@@ -0,0 +1,115 @@
+#HW 06
+
+install.packages("deSolve")
+library(deSolve)
+
+############################################################
+# FUNCTION: run_sir_model
+# PURPOSE: Solve deterministic SIR model with deSolve
+# INPUTS:
+#   beta, gamma : transmission & recovery rates
+#   N           : total population
+#   S0, I0, R0  : initial conditions
+#   t_start     : start time
+#   t_end       : end time
+#   dt          : timestep
+# OUTPUT:
+#   data.frame with columns: time, S, I, R
+############################################################
+
+run_sir_model <- function(
+  beta    = 0.1,
+  gamma   = 0.1,
+  N       = 1000,
+  S0      = 999,
+  I0      = 1,
+  R0      = 0,
+  t_start = 0,
+  t_end   = 160,
+  dt      = 1
+) {
+
+  init  <- c(S = S0, I = I0, R = R0)
+  times <- seq(t_start, t_end, by = dt)
+
+  sir_equations <- function(time, state, parameters) {
+    with(as.list(c(state, parameters)), {
+      dS <- -beta * S * I / N
+      dI <-  beta * S * I / N - gamma * I
+      dR <-  gamma * I
+      list(c(dS, dI, dR))
+    })
+  }
+
+  out <- ode(
+    y     = init,
+    times = times,
+    func  = sir_equations,
+    parms = c(beta = beta, gamma = gamma, N = N)
+  )
+
+  as.data.frame(out)
+}
+
+
+############################## BETA AND GAMMA FOR LOOP FUNCTION ######################
+
+
+
+
+
+b_g_I <- function(beta=seq(0,0.5,by=0.01), gamma=seq(0,0.5,by=0.01)){
+
+  # row indexing variable
+  counter <- 1
+
+  # create storage dataframe
+  dfLength <- length(beta)*length(gamma) # how long is DF
+  b_out <- rep(NA, dfLength)
+  g_out <- rep(NA, dfLength)
+  maxi_out <- rep(NA, dfLength)
+
+  # turn vecs into df
+  storageDF <- data.frame(b_out, g_out, maxi_out)
+
+
+  # enter our loops
+  for (i in seq_along(beta)){ # rows
+    for (j in seq_along(gamma)){ # cols
+
+      # run sir model
+      tmp_df <- run_sir_model(beta = beta[i], gamma = gamma[j])
+
+      # store max i in dataframe
+      storageDF$maxi_out[counter] <- max(tmp_df$I)
+      storageDF$b_out[counter] <- beta[i] # value for b stored
+      storageDF$g_out[counter] <- gamma[j] # value for g stored
+
+      counter <- counter + 1 # increase counter
+    }
+  }
+  return(storageDF)
+}
+
+result<-b_g_I()
+result
+
+
+# run growth param sweep
+df <- growth_sweep(rvec = r_values, kvec = K_values)
+
+df
+
+
+
+## Question 2
+
+ggplot(data = df, aes(x = g_out, y = b_out, fill= maxn_out)) + 
+  geom_tile()
+
+
+
+
+
+
+
--- r_scripts/for_loops.R
+++ r_scripts/for_loops.R
@@ -1,156 +1,173 @@
-# for_loops basic stuff 
-#10 Feb. 2026 
-#BEW
 
-#create basic for loop
-for (i in 1:5) {
-  cat("stuck in a loop","\n")
-  cat(3+2,"\n")
-  cat(runif(1), "\n")
-}
+# Working with for loops
+# P. Alexander Burnham
+# 12 February 2026
 
-my_dogs<- c("chow", "akita", "malamute", "husky", "samoyed")
-
-for (i in 1:length(my_dogs)){
-  cat("i = ", i, "my_dogs[i]", my_dogs[i], "\n")
-}
-
-
-my_bad_dogs <- NULL
-
-for(i in 1:length(my_bad_dogs)){
-  cat("i =", i, "my_bad_dogs[i]", my_bad_dogs[i], "\n")
-}
-
-for(i in seq_along(my_dogs)){
-  cat("i =", i, "my_bad_dogs[i]", my_bad_dogs[i], "\n")
-}
-
-
-# Tip 1:
-#don't do things in a loop that you dont need to: e.g.:
-
-for(i in seq_along(my_dogs)){
-  my_dogs[i]<- toupper(my_dogs[i]) #doesn't need to be in a loop 
-}
-
-tolower(my_dogs) #can just do the operation in the for loop above using a simple function <--
-
-
-
-#Tip 2:
-# don't change dimensions in the loop e.g.:
-
-my_dat<-runif(1)
-
-for(i in 2:10){
-  temp<-runif(1)
-  my_dat<-c(my_dat,10)
-  #cat("loop number =", i, my_dat[1], "\n")
-  print(my_dat)
-}
-
-
-#tip 3:
-#don't write a loop if you can vectorize it
-
-my_dat <- 1:10 
-for (i in seq_along(my_dat)){
-  my_dat[i]<- my_dat[i]+my_dat[1^2]
-  cat("loop number =", i, my_dat[1], "\n")
-}
-
-#do this instead of the for loop above
-
-z <- 1:10
-z<- z + z^2
-z
-
-# TIP 4: 
-# remember difference btwn i and z[i]m ("ith element of z")
-
-z<- c(10,2,4)
-for(i in seq_along(z)){
-  cat("i=", i, "z[i]=", z[i], "\n")
-}
-
-#tip 5
-#don't have to loop through everything
-
-z<- 1:20 
-for(i in seq_along(z)){
-  if(i %% 2 ==0) next # if i is not divisible evenly by two, then skip that
-  print(i)
+# LOGISTIC GROWTH FUNCTION
+######################################################
+# Function Name: logistic_growth
+#
+# Purpose:
+#   Simulates continuous-time logistic population growth using the
+#   closed-form solution to the logistic differential equation.
+#   Returns a tidy dataframe suitable for plotting or further analysis.
+#
+# Inputs:
+#   N0 (numeric)  : Initial population size at time t = 0
+#   r  (numeric)  : Intrinsic growth rate
+#   K  (numeric)  : Carrying capacity
+#   t_max (numeric) : Maximum simulation time
+#   dt (numeric)  : Time step used to generate the time vector
+#
+# Output:
+#   data.frame containing:
+#     time (numeric)        : Time values from 0 to t_max
+#     population (numeric)  : Population size N(t) at each time
+#     N0 (numeric)          : Initial population parameter used
+#     r (numeric)           : Growth rate parameter used
+#     K (numeric)           : Carrying capacity parameter used
+######################################################
+logistic_growth <- function(
+  N0 = 10,
+  r  = 0.3,
+  K  = 100,
+  t_max = 50,
+  dt = 0.1
+){
   
+  # time vector
+  time <- seq(0, t_max, by = dt)
+  
+  # logistic equation (closed-form solution)
+  N <- K / (1 + ((K - N0) / N0) * exp(-r * time))
+  
+  # return tidy dataframe
+  data.frame(
+    time = time,
+    population = N,
+    N0 = N0,
+    r = r,
+    K = K
+  )
+}
+######################################################
+# END FUNCTION
+
+# 1. Initialize vector of 1000 r values from 0 to 1
+r_values <- seq(0, 1, length.out = 1000)
+
+# 2. Initialize storage container
+max_n_values <- numeric(length(r_values))
+
+# 3. Loop through r values
+for (i in seq_along(r_values)) {
+  temp <- logistic_growth(r = r_values[i])
+  max_n_values[i] <- max(temp$population)
 }
 
-###########################################
+# make data frame
+df <- data.frame(r_values, max_n_values)
+names(df) <- c("r", "max(N)")
 
-# look at the parameter space of the logistic growth model w a for loop 
+
+plot(x = df$r, y = df$`max(N)`)
 
 
-################ Function Start ########################
-#FUNCTION NAME: my_func
-#PURPOSE: function returns a df with columns for pop size and time
-#INPUTS: initial pop, growth rate, carrying capacity, end time of time vector,timestep for time vector
-#OUTPUTS: #data frame with pop size and time
 
-my_func<-function(No = 5, r = 0.5, K = 75, tf=100, ts= 1){ 
-    e <- 2.718281828459 #define e constant
-    t<- seq(from= 1,to=tf,by = ts) #define the number of steps, store as vector
-    n<- K/(1+((K-No)/No)*e^(-r*t)) # formula to calculate the N(t) 
-    out_df<- data.frame(t,n) # create df
-    return(out_df)
+
+
+##################################
+# 2D parameter sweep for log growth function
+# outputs matrix
+
+
+# vectors for r and k parameters
+r_values <- seq(0, 1, length.out = 100)
+K_values <- seq(10, 1000, length.out = 100)
+
+
+growth_sweep <- function(rvec, kvec){
+
+  # create storage matrix for outputs
+  stor_mat <- matrix(NA, nrow = length(rvec), ncol = length(kvec))
+
+  for (i in seq_along(rvec)){ # rows
+    for (j in seq_along(kvec)){ # cols
+
+      # run log growth
+      tmp_df <- logistic_growth(r = rvec[i], K = kvec[j])
+
+      # store max n in 2d matrix
+      stor_mat[i,j] <- max(tmp_df$population)
     }
+  }
+  return(stor_mat)
+}
 
-df<-my_func() #run function, assign to df
+# run growth param sweep
+growth_mat <- growth_sweep(rvec = r_values, kvec = K_values)
+
+
+
+
+
+
+
+
+
+##################################
+# 2D parameter sweep for log growth function
+# outputs a data frame
+
+numParms <- 100
+
+
+# vectors for r and k parameters
+r_values <- seq(0, 1, length.out = numParms)
+K_values <- seq(10, 1000, length.out = numParms)
+
+
+
+growth_sweep <- function(rvec, kvec){
+
+  # row indexing variable
+  counter <- 1
+
+  # create storage dataframe
+  dfLength <- length(rvec)*length(kvec) # how long is DF
+  r_out <- rep(NA, dfLength)
+  k_out <- rep(NA, dfLength)
+  maxn_out <- rep(NA, dfLength)
+
+  # turn vecs into df
+  storageDF <- data.frame(r_out, k_out, maxn_out)
+
+
+  # enter our loops
+  for (i in seq_along(rvec)){ # rows
+    for (j in seq_along(kvec)){ # cols
+
+      # run log growth
+      tmp_df <- logistic_growth(r = rvec[i], K = kvec[j])
+
+      # store max n in dataframe
+      storageDF$maxn_out[counter] <- mean(tmp_df$population)
+      storageDF$r_out[counter] <- rvec[i] # value for r stored
+      storageDF$k_out[counter] <- kvec[j] # value for k stored
+
+      counter <- counter + 1 # increase counter
+    }
+  }
+  return(storageDF)
+}
+
+# run growth param sweep
+df <- growth_sweep(rvec = r_values, kvec = K_values)
+
 df
 
-
-r_vec <- seq(0,1, by = 0.01) #vector of little rs 
-r_vec
-
-container_vec <- rep(NA,length(r_vec)) #storage for max(n)
-c(length(container_vec), length(r_vec)) #both 21
-container_vec
-
-
-
-for (i in seq_along(r_vec)){
-  temp_df<- my_func(r=r_vec[i]) #draws on hw 5 function (logistic growth)
-  max_n<-max(temp_df$n)
-  container_vec[i] <- max_n #storage is happening here
- 
-}
-
-growth_df <- data.frame(r= r_vec,n = container_vec) 
-plot(x= growth_df$r,y=growth_df$n) 
-
-############
-#2D parameter sweep for log growth func
-
-r_values<- seq(0,1, length,out = 100)
-K_values<- seq(10,1000, length,out = 100)
-
-store_mat<- matrix (NA,nrow = length(r_values), ncol = length(K_values))
-
-my_func(r=r_values[i],K = K_values)
-
-
-
-growth_mat<- growth_sweep(r_vec = r_values,kvec=K_values)
-
-growth_mat <- 
-
-
-######## missed a ton of shit, None of the above will work^^^^^^
-  
-  
-#2D parameter sweeep output Data Fram
-for (i in seq_along(r_vec)){
-
-  
-}
+ggplot(data = df, aes(x = r_out, y = k_out, fill= maxn_out)) + 
+  geom_tile()
 
 
 
@@ -158,55 +175,53 @@ for (i in seq_along(r_vec)){
 
 
 
-#random walk func
 
-###########
-#Name; 
-#purpose
-#input : times = number of time steps 
-#         n1    = inital pop size 
-#          lamda = finite rate of increase
-#        noise_sd= 10 
-# output: 
-#         vector n with pop size >0 until extinct 
+
+# create a random walk function
+
+###############################
+# NAME: ran_walk
+# purpose: conducts a random walk
+# input:times = number of time steps
+#       n1 = initial pop size
+#       lamda = finite rate of increase
+#.      noise_sd = 10
+# output:
+#          vector n with population size > 0 until extinct.
 #
 library(ggplot2)
 
-ran_walk <- function(times=100, n1= 50 ,lamda = 1, noise_sd=10){
-  n <- rep(NA,times) #create our output vec ("storage container")
-  n[1]<- n1 #initialize init pop size
-  noise <- rnorm(n=100,mean =0, sd = noise_sd) #created noise/error 
 
-  for(i in 1:(times-1)){ #do times-1 because we already defined n[1] as initial pop
-    n[i+1] <- lamda*n[i] + noise_sd[i]
-    if(n[i+1] <= 0){
-      n[i+1] <- NA
-      cat("Population Extinction at time", i+1, "\n")
-      break 
+
+ran_walk <- function(times = 100, n1=50, lamda = 1, noise_sd = 10){
+  n <- rep(NA, times) # create our output vec
+  n[1] <- n1 # initialize init pop size
+  noise <- rnorm(n = 100, mean = 0, sd = noise_sd) # created noise/error
+
+  for (i in 1:(times-1)){
+    n[i + 1] <- lamda*n[i] + noise[i]
+    if(n[i + 1] <= 0){
+      n[i + 1] <- NA
+      cat("Population extinction at time", i+1, "\n")
+      break
     }
   }
   return(n)
-
 }
 
-x<- ran_walk()
+x <- ran_walk()
 print(x)
 
-#plotting with default values
-qplot(x = 1:100,y=ran_walk(), geom="line")
-
+# plotting with default values
+qplot(x=1:100, y=ran_walk(), geom="line")
 
 # no noise/not so random walk
-qplot(x = 1:100,y=ran_walk(noise_sd=0), geom="line")
+qplot(x=1:100, y=ran_walk(noise_sd=0), geom="line")
 
+# no noise and adjust lamda
+qplot(x=1:100, y=ran_walk(lamda = 0.92, noise_sd=0), geom="line")
 
-#no noise and adjust lamda
-
-qplot(x = 1:100,y=ran_walk(lamda = 0.92, noise_sd=0), geom="line")
-
-
-#add some stoch back, make lamda>1
-
-qplot(x = 1:100,y=ran_walk(lamda = 1.11, noise_sd=10), geom="line")
+# add some stoch. make lamda > 1
+qplot(x=1:100, y=ran_walk(lamda = 1.01, noise_sd=6), geom="line")
 
 
